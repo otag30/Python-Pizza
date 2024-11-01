3e7fc08487755f3a4febcbf0a65f260b71df523d
Carmen <aisofiacatarino@gmail.com>
2024-11-01 10:09:19 +0000

    added README.md file and exercise code

---
+++ main.py
@@ -0,0 +1,28 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+final_bill = 0
+
+if size == "S":
+    final_bill += 15
+    if pepperoni == "Y":
+        final_bill += 2
+    if extra_cheese == "Y":
+        final_bill += 1
+
+elif size == "M":
+    final_bill += 20
+    if pepperoni == "Y":
+        final_bill += 3
+    if extra_cheese == "Y":
+        final_bill += 1
+else:
+    final_bill = 25
+    if pepperoni == "Y":
+        final_bill += 3
+    if extra_cheese == "Y":
+        final_bill += 1
+
+print(f"Your final bill is: £{final_bill}.")
